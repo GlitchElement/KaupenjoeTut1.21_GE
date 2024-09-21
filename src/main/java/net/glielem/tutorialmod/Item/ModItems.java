a79739d2271a183afb6fe2087b9cb0eb597d58e8
GlitchElement <glitchelementyt@gmail.com>
2024-09-21 01:19:50 -0400

    Episode 8

--- src/main/java/net/glielem/tutorialmod/Item/ModItems.java
+++ src/main/java/net/glielem/tutorialmod/Item/ModItems.java
@@ -1,6 +1,7 @@
 package net.glielem.tutorialmod.Item;
 
 import net.glielem.tutorialmod.Item.custom.ChiselItem;
+import net.glielem.tutorialmod.Item.custom.FuelItem;
 import net.glielem.tutorialmod.TutorialMod;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,6 +21,12 @@ public class ModItems {
 
     public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
             () -> new ChiselItem(new Item.Properties().durability(32)));
+
+    public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi",
+            () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
+
+    public static final RegistryObject<Item> AURORA_ASHES = ITEMS.register("aurora_ashes",
+            () -> new FuelItem(new Item.Properties(), 1200));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
