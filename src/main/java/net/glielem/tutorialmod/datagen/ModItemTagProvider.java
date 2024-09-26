0f7da1d14b824aad43b4b3c090c0ee4eea9a34f5
GlitchElement <glitchelementyt@gmail.com>
2024-09-25 23:52:49 -0400

    Episode 11

---
+++ src/main/java/net/glielem/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,33 @@
+package net.glielem.tutorialmod.datagen;
+
+import net.glielem.tutorialmod.Item.ModItems;
+import net.glielem.tutorialmod.TutorialMod;
+import net.glielem.tutorialmod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+
+
+    public ModItemTagProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> providerCompletableFuture,
+                              CompletableFuture<TagLookup<Block>> tagLookupCompletableFuture, @Nullable ExistingFileHelper existingFileHelper) {
+        super(packOutput, providerCompletableFuture, tagLookupCompletableFuture, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.ALEXANDRITE.get())
+                .add(ModItems.RAW_ALEXANDRITE.get())
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.COMPASS);
+    }
+}
