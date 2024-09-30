3d423337500adca73d25c52cc2fb16846dcdea4e
GlitchElement <glitchelementyt@gmail.com>
2024-09-30 00:53:48 -0400

    Episode 15

--- src/main/java/net/glielem/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/glielem/tutorialmod/datagen/ModItemModelProvider.java
@@ -5,6 +5,7 @@ import net.glielem.tutorialmod.TutorialMod;
 import net.glielem.tutorialmod.block.ModBlocks;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
@@ -34,14 +35,26 @@ public class ModItemModelProvider extends ItemModelProvider {
         wallItem(ModBlocks.ALEXANDRITE_WALL, ModBlocks.ALEXANDRITE_BLOCK);
 
         simpleBlockItem(ModBlocks.ALEXANDRITE_DOOR);
+
+        handheldItem(ModItems.ALEXANDRITE_SWORD);
+        handheldItem(ModItems.ALEXANDRITE_PICKAXE);
+        handheldItem(ModItems.ALEXANDRITE_AXE);
+        handheldItem(ModItems.ALEXANDRITE_SHOVEL);
+        handheldItem(ModItems.ALEXANDRITE_HOE);
     }
 
 //--------------------------------------------------------------------------------------------------------------------
-public void buttonItem(RegistryObject<? extends Block> block, RegistryObject<Block> baseBlock) {
-    this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
-            .texture("texture",  ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,
-                    "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
-}
+    private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                ResourceLocation.parse("item/handheld")).texture("layer0",
+                ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,"item/" + item.getId().getPath()));
+    }
+
+    public void buttonItem(RegistryObject<? extends Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
+                .texture("texture",  ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,
+                        "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
 
     public void fenceItem(RegistryObject<? extends Block> block, RegistryObject<Block> baseBlock) {
         this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
